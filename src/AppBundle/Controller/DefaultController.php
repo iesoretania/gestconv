9cb293a2e80be1b5448ce75f016bcc6af85d5237
Luis Ramón López <lrlopez@gmail.com>
2015-01-17 20:45:26 +0100

    Añadido método a la ruta por defecto

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,7 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="portada",methods={"GET"})
      */
     public function indexAction()
     {
