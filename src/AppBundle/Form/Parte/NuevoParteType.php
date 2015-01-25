3a84e8a171951c1c487c2566b4dc1d314f2f2169
Luis Ramón López <lrlopez@gmail.com>
2015-01-25 22:38:28 +0100

    Personalizado el formulario de nuevo parte

--- src/AppBundle/Form/Parte/NuevoParteType.php
+++ src/AppBundle/Form/Parte/NuevoParteType.php
@@ -16,23 +16,49 @@ class NuevoParteType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('usuario')
-            ->add('fechaCreacion')
-            ->add('fechaSuceso')
-            ->add('alumnos', null, array(
+            ->add('usuario', null, [
+                'label' => 'Docente'
+            ])
+            ->add('fechaCreacion', null, [
+                'label' => 'Parte creado'
+            ])
+            ->add('fechaSuceso', null, [
+                'label' => 'Fecha y hora del suceso',
+                'required'  => true
+            ])
+
+            ->add('tramo', null, [
+                'label' => 'Dónde ha sucedido',
+                'required'  => true
+            ])
+            ->add('alumnos', null, [
                 'query_builder' => function(EntityRepository $er) {
                     return $er->createQueryBuilder('u')
                         ->orderBy('u.apellido1', 'ASC')
                         ->addOrderBy('u.apellido2', 'ASC')
                         ->addOrderBy('u.nombre', 'ASC');
-                }))
-            ->add('anotacion')
-            ->add('actividades')
-            ->add('fechaAviso')
-            ->add('prescrito')
-            ->add('profesorGuardia')
-            ->add('tramo')
-            ->add('sancion')
+                },
+                'label' => 'Alumnado implicado',
+                'required'  => true
+            ])
+            ->add('anotacion', 'textarea', [
+                'label' => 'Detalle de lo acontecido',
+                'attr' => ['rows' => '8']
+
+            ])
+            ->add('actividades', 'textarea', [
+                'label' => 'Actividades a realizar',
+                'attr' => ['rows' => '5']
+            ])
+            ->add('fechaAviso', null, [
+                'label' => 'Fecha de aviso a las familias'
+            ])
+            ->add('prescrito', null, [
+                'label' => '¿Ha prescrito?'
+            ])
+            ->add('profesorGuardia', null, [
+                'label' => 'Profesor de guardia'
+            ])
         ;
     }
 
