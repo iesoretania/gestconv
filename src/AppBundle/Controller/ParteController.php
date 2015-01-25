bf4c19d6a606e8a160b38b9c932a9fa57f97e913
Luis Ramón López <lrlopez@gmail.com>
2015-01-25 23:37:46 +0100

    Modificada la carpeta en nuevo parte

--- src/AppBundle/Controller/ParteController.php
+++ src/AppBundle/Controller/ParteController.php
@@ -3,7 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Parte;
-use AppBundle\Form\Parte\NuevoParteType;
+use AppBundle\Form\Type\NuevoParteType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
