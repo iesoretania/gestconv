6d1667892ddd92b8ace9fb5f68b4c327f7884eb6
Luis Ramón López <lrlopez@gmail.com>
2015-01-26 00:40:11 +0100

    Mostrados los mensaje de error en la entrada

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,8 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContext;
 
 class DefaultController extends Controller
 {
@@ -18,8 +20,21 @@ class DefaultController extends Controller
     /**
      * @Route("/entrar", name="usuario_entrar",methods={"GET", "POST"})
      */
-    public function entrarAction()
+    public function entrarAction(Request $peticion)
     {
-        return $this->render('AppBundle:App:entrada.html.twig');
+        $sesion = $peticion->getSession();
+
+        if ($peticion->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $peticion->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        }
+        else {
+            $error = $sesion->get(SecurityContext::AUTHENTICATION_ERROR);
+            $sesion->remove(SecurityContext::AUTHENTICATION_ERROR);
+        }
+        return $this->render('AppBundle:App:entrada.html.twig',
+            [
+                'last_username' => $sesion->get(SecurityContext::LAST_USERNAME),
+                'error' => $error
+            ]);
     }
 }
