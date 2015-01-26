d6f2a2aa6a49f4df8d16992cd515ccb7c11b7894
Luis Ramon Lopez <lrlopez@gmail.com>
2015-01-26 13:13:24 +0100

    Ajustado SecurityContext a los cambios de Symfony 2.6

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,7 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\Security\Core\Security;
 
 class DefaultController extends Controller
 {
@@ -24,16 +24,16 @@ class DefaultController extends Controller
     {
         $sesion = $peticion->getSession();
 
-        if ($peticion->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
-            $error = $peticion->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        if ($peticion->attributes->has(Security::AUTHENTICATION_ERROR)) {
+            $error = $peticion->attributes->get(Security::AUTHENTICATION_ERROR);
         }
         else {
-            $error = $sesion->get(SecurityContext::AUTHENTICATION_ERROR);
-            $sesion->remove(SecurityContext::AUTHENTICATION_ERROR);
+            $error = $sesion->get(Security::AUTHENTICATION_ERROR);
+            $sesion->remove(Security::AUTHENTICATION_ERROR);
         }
         return $this->render('AppBundle:App:entrada.html.twig',
             [
-                'last_username' => $sesion->get(SecurityContext::LAST_USERNAME),
+                'last_username' => $sesion->get(Security::LAST_USERNAME),
                 'error' => $error
             ]);
     }
