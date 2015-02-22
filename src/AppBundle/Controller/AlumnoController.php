d1213e7acfe641fe8677d3d000f3b4d0b8e34772
Luis Ramón López <lrlopez@gmail.com>
2015-02-22 22:57:55 +0100

    Eliminados imports no utilizados

--- src/AppBundle/Controller/AlumnoController.php
+++ src/AppBundle/Controller/AlumnoController.php
@@ -8,7 +8,6 @@ use AppBundle\Form\Type\RangoFechasType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
