e61943f1ca39e39632729d019c6e2994894f4c1d
Luis Ramón López <lrlopez@gmail.com>
2015-03-05 23:00:19 +0100

    Eliminados DataFixtures de datos inventados

--- src/AppBundle/DataFixtures/ORM/Usuarios.php
+++ src/AppBundle/DataFixtures/ORM/Usuarios.php
@@ -20,54 +20,26 @@
 
 namespace AppBundle\DataFixtures\ORM;
 
-use AppBundle\DataFixtures\Utils\Nombres;
 use AppBundle\Entity\Usuario;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class Usuarios extends AbstractFixture implements OrderedFixtureInterface
+class Usuarios extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
 {
-    protected $faker;
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
 
     /**
-     * Rellena los datos de un usuario aleatoriamente
-     *
-     * @return Usuario
+     * {@inheritDoc}
      */
-    public function generateUsuario()
+    public function setContainer(ContainerInterface $container = null)
     {
-        $usuario = new Usuario();
-        // Generar tipo de alumno (0=H, 1=M)
-        $tipo = rand(0, 1);
-
-        $nombre = Nombres::generateNombreCompleto($tipo);
-
-        $usuario->setNombre($nombre['nombre']);
-        $apellidos = $nombre['apellido1'];
-        // A veces no generar un segundo apellido
-        if (rand(0,100) > 0) {
-            $apellidos .= ' ' . $nombre['apellido2'];
-        }
-        $usuario->setApellidos($apellidos);
-
-        $username = $nombre['nombre'][0] . mb_substr($nombre['apellido1'], 0, 3, 'UTF-8') .
-            mb_substr($nombre['apellido2'], 0, 3, 'UTF-8');
-        $username = mb_strtolower($username, 'UTF-8');
-        $username .= rand(0, 9) . rand(0, 9) . rand(0, 9);
-
-        $busqueda = ['á', 'é', 'í', 'ó', 'ú', 'ñ'];
-        $cambiarPor = ['a', 'e', 'i', 'o', 'u', 'n'];
-        $username = str_replace($busqueda, $cambiarPor, $username);
-
-        $usuario->setNombreUsuario($username);
-        if (rand(0, 4)>0) {
-            $usuario->setEmail($this->faker->email);
-        }
-
-        $usuario->setPassword(password_hash('12345', PASSWORD_DEFAULT));
-
-        return $usuario;
+        $this->container = $container;
     }
 
     public function getOrder()
@@ -77,11 +49,11 @@ class Usuarios extends AbstractFixture implements OrderedFixtureInterface
 
     public function load(ObjectManager $manager)
     {
-        $this->faker = \Faker\Factory::create('es_ES');
+        $encoder = $this->container->get('security.password_encoder');
 
         $usuario = new Usuario();
         $usuario->setNombreUsuario('admin')
-            ->setPassword(password_hash('admin', PASSWORD_DEFAULT))
+            ->setPassword($encoder->encodePassword($usuario, 'admin'))
             ->setNombre('Administrador')
             ->setApellidos('Admin')
             ->setEsAdministrador(true)
@@ -92,42 +64,7 @@ class Usuarios extends AbstractFixture implements OrderedFixtureInterface
 
         $manager->persist($usuario);
 
-        $usuario = new Usuario();
-        $usuario->setNombreUsuario('comisionario')
-            ->setPassword(password_hash('comisionario', PASSWORD_DEFAULT))
-            ->setNombre('Comisión')
-            ->setApellidos('Convivencia')
-            ->setEsAdministrador(false)
-            ->setEsRevisor(true)
-            ->setEsDirectivo(false)
-            ->setEstaActivo(true)
-            ->setEstaBloqueado(false);
-
-        $manager->persist($usuario);
-
-        $usuario = new Usuario();
-        $usuario->setNombreUsuario('usuario')
-            ->setPassword(password_hash('usuario', PASSWORD_DEFAULT))
-            ->setNombre('Juan')
-            ->setApellidos('Nadie')
-            ->setEsAdministrador(false)
-            ->setEsRevisor(false)
-            ->setEsDirectivo(false)
-            ->setEstaActivo(true)
-            ->setEstaBloqueado(false);
-
-        $manager->persist($usuario);
-
-        for ($i = 0; $i < rand(30, 45); $i++) {
-            $usuario = self::generateUsuario();
-            $usuario->setEsAdministrador(false)
-                ->setEsRevisor(rand(1, 20) > 19)
-                ->setEsDirectivo(false)
-                ->setEstaActivo(true)
-                ->setEstaBloqueado(false);
-            $manager->persist($usuario);
-        }
-
         $manager->flush();
     }
+
 }
