4bc41a646df243da90dfd18c6f7410078606c354
Luis Ramón López <lrlopez@gmail.com>
2015-03-14 22:31:38 +0100

    Reemplazadas comillas dobles por simples en los datos iniciales

--- src/AppBundle/DataFixtures/ORM/DatosIniciales.php
+++ src/AppBundle/DataFixtures/ORM/DatosIniciales.php
@@ -31,12 +31,12 @@ class DatosIniciales extends AbstractFixture implements OrderedFixtureInterface
     public function generateTramoParte(ObjectManager $manager)
     {
         $tramos = [
-            "En clase",
-            "En el intercambio de clases",
-            "Entrada/Salida",
-            "Recreo",
-            "Durante las actividades extraescolares",
-            "Otros"
+            'En clase',
+            'En el intercambio de clases',
+            'Entrada/Salida',
+            'Recreo',
+            'Durante las actividades extraescolares',
+            'Otros'
         ];
 
         foreach($tramos as $descripcion) {
@@ -80,27 +80,27 @@ class DatosIniciales extends AbstractFixture implements OrderedFixtureInterface
         $conductas = [
             'Conductas contrarias' =>
                 [
-                    "Perturbación del normal desarrollo de las actividades de clase",
-                    "Falta de colaboración sistemática en la realización de las actividades",
-                    "Impedir o dificultar el estudio a sus compañeros",
-                    "Faltas injustificadas de puntualidad",
-                    "Faltas injustificadas de asistencia a clase",
-                    "Actuaciones incorrectas hacia algún miembro de la comunidad educativa",
-                    "Daños en instalaciones o docum. del Centro o en pertenencias de un miembro"
+                    'Perturbación del normal desarrollo de las actividades de clase',
+                    'Falta de colaboración sistemática en la realización de las actividades',
+                    'Impedir o dificultar el estudio a sus compañeros',
+                    'Faltas injustificadas de puntualidad',
+                    'Faltas injustificadas de asistencia a clase',
+                    'Actuaciones incorrectas hacia algún miembro de la comunidad educativa',
+                    'Daños en instalaciones o docum. del Centro o en pertenencias de un miembro'
                 ],
             'Conductas graves' =>
                 [
-                    "Agresión física a un miembro de la comunidad educativa",
-                    "Injurias y ofensas contra un miembro de la comunidad educativa",
-                    "Acoso escolar",
-                    "Actuaciones perjudiciales para la salud y la integridad, o incitación a ellas",
-                    "Vejaciones o humillaciones contra un miembro de la comunidad educativa",
-                    "Amenazas o coacciones a un miembro de la comunidad educativa",
-                    "Suplantación de la personalidad, y falsificación o sustracción de documentos",
-                    "Deterioro grave de instalac. o docum. del Centro, o pertenencias de un miembro",
-                    "Reiteración en un mismo curso de conductas contrarias a normas de convivencia",
-                    "Impedir el normal desarrollo de las actividades del centro",
-                    "Incumplimiento de las correcciones impuestas"
+                    'Agresión física a un miembro de la comunidad educativa',
+                    'Injurias y ofensas contra un miembro de la comunidad educativa',
+                    'Acoso escolar',
+                    'Actuaciones perjudiciales para la salud y la integridad, o incitación a ellas',
+                    'Vejaciones o humillaciones contra un miembro de la comunidad educativa',
+                    'Amenazas o coacciones a un miembro de la comunidad educativa',
+                    'Suplantación de la personalidad, y falsificación o sustracción de documentos',
+                    'Deterioro grave de instalac. o docum. del Centro, o pertenencias de un miembro',
+                    'Reiteración en un mismo curso de conductas contrarias a normas de convivencia',
+                    'Impedir el normal desarrollo de las actividades del centro',
+                    'Incumplimiento de las correcciones impuestas'
                 ],
             'Otras conductas contrarias' =>
                 [
@@ -117,28 +117,28 @@ class DatosIniciales extends AbstractFixture implements OrderedFixtureInterface
         $medidas = [
             'Correciones a las conductas contrarias' =>
                 [
-                    "Amonestación oral",
-                    "Apercibimiento por escrito",
-                    "Realizar tareas dentro y fuera del horario lectivo en el Centro",
-                    "Suspender el derecho de asistencia a determinadas clases entre 1 y 3 días",
-                    "Suspender el derecho de asistencia al centro entre 1 y 3 días",
+                    'Amonestación oral',
+                    'Apercibimiento por escrito',
+                    'Realizar tareas dentro y fuera del horario lectivo en el Centro',
+                    'Suspender el derecho de asistencia a determinadas clases entre 1 y 3 días',
+                    'Suspender el derecho de asistencia al centro entre 1 y 3 días',
 
                 ],
             'Medidas disciplinarias por conductas gravemente perjudiciales' =>
                 [
-                    "Realizar tareas fuera del horario lectivo en el Centro",
-                    "Suspender el derecho de participación en actividades extraescolares del Centro",
-                    "Suspender el derecho de asistencia a determinadas clases entre 4 y 14 días",
-                    "Suspender el derecho de asistencia al centro entre 4 y 30 días",
-                    "Cambio de centro docente",
+                    'Realizar tareas fuera del horario lectivo en el Centro',
+                    'Suspender el derecho de participación en actividades extraescolares del Centro',
+                    'Suspender el derecho de asistencia a determinadas clases entre 4 y 14 días',
+                    'Suspender el derecho de asistencia al centro entre 4 y 30 días',
+                    'Cambio de centro docente'
                 ],
             'Otras medidas' =>
                 [
-                    "Mediación escolar",
-                    "Aula de convivencia",
-                    "Tutoría compartida",
-                    "Compromiso de convivencia",
-                    "Otras incluidas en el plan de convivencia"
+                    'Mediación escolar',
+                    'Aula de convivencia',
+                    'Tutoría compartida',
+                    'Compromiso de convivencia',
+                    'Otras incluidas en el plan de convivencia'
                 ]
         ];
 
@@ -163,12 +163,12 @@ class DatosIniciales extends AbstractFixture implements OrderedFixtureInterface
     public function generateCategoriasAviso(ObjectManager $manager)
     {
         $categorias = [
-            "Llamada telefónica",
-            "Mensaje en el contestador",
-            "SMS",
-            "Entrevista personal",
-            "Correo electrónico",
-            "Otro (detallar)"
+            'Llamada telefónica',
+            'Mensaje en el contestador',
+            'SMS',
+            'Entrevista personal',
+            'Correo electrónico',
+            'Otro (detallar)'
         ];
 
         self::createItems($manager, $categorias, 'CategoriaAviso');
@@ -177,9 +177,9 @@ class DatosIniciales extends AbstractFixture implements OrderedFixtureInterface
     public function generateActitudFamilia(ObjectManager $manager)
     {
         $actitudes = [
-            "Colabora",
-            "No colabora",
-            "Impide la corrección"
+            'Colabora',
+            'No colabora',
+            'Impide la corrección'
         ];
 
         self::createItems($manager, $actitudes, 'ActitudFamiliaSancion');
