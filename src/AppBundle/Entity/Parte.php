71b8f6aa031b80850cfe7ba7769098c7d208461e
Luis Ramón López <lrlopez@gmail.com>
2016-01-28 22:32:18 +0100

    Añadido un import olvidado en Parte

--- src/AppBundle/Entity/Parte.php
+++ src/AppBundle/Entity/Parte.php
@@ -25,6 +25,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use AppBundle\Validator\Constraints\DateRange as AssertDateRange;
 
 
 /**
